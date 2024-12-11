be397b1ff8143516285edbbbf8cc99792509c489
hourhl <hourhl8200@gmail.com>
2024-12-11 14:51:44 +0800

    module : payment; status : unit test pass

--- app/payment/biz/service/charge_test.go
+++ app/payment/biz/service/charge_test.go
@@ -2,20 +2,38 @@ package service
 
 import (
 	"context"
-	"testing"
+	"fmt"
+	"github.com/hourhl/Qmall/app/payment/biz/dal"
 	payment "github.com/hourhl/Qmall/rpc_gen/kitex_gen/payment"
+	"github.com/joho/godotenv"
+	"testing"
 )
 
 func TestCharge_Run(t *testing.T) {
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		fmt.Printf("loading .env file error : %v\n", err.Error())
+	}
+	dal.Init()
 	ctx := context.Background()
 	s := NewChargeService(ctx)
 	// init req and assert value
 
-	req := &payment.ChargeReq{}
+	req := &payment.ChargeReq{
+		UserId:  uint32(1),
+		OrderId: "46f2420c-b545-11ef-b539-28c5c873c72b",
+		Amount:  float32(200),
+		CreditCard: &payment.CreditCardInfo{
+			CreditCardNumber:          "4111111111111111",
+			CreditCardCvv:             123,
+			CreditCardExpirationMonth: 12,
+			CreditCardExpirationYear:  2100,
+		},
+	}
 	resp, err := s.Run(req)
 	t.Logf("err: %v", err)
 	t.Logf("resp: %v", resp)
 
-	// todo: edit your unit test
+	// status : PASS
 
 }
